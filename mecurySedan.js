3aed0ca655b9fb4e5e04cd9947f9bb9e33e1828d
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-06-07 10:56:57 -0400

    finished activity

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,31 +1,56 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicle.js")
+const VehicleModule = require('./vehicle').Vehicle
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
-
-class Car extends VehicleModule.Vehicle {
-    constructor(maxPassengers, passenger, numWheels, maxSpeed, fuel, schedService) {
-        super(this.make, this.model, this.year, this.color, this.mileage);
-        this.maxPassengers = maxPassengers;
-        this.passenger = passenger;
-        this.numberOfWheels = numWheels;
-        this.maxSpeed = maxSpeed;
-        this.fuel = fuel;
-        this.schedService = schedService;
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
     }
-    loadPassenger() {
 
+    checkService() {
+        if (this.mileage > 30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+        }
     }
+
     start() {
-
+        if (this.fuel > 0) {
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
     }
-    schedService() {
 
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + " does not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
     }
+
+
 }
 
-let A28Mercury = new Car(5, 0, 4, 160, 10, false)
 
-console.log(A28Mercury)
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(6)
+myCar.loadPassenger()
+myCar.stop()
+myCar.checkService()
+console.log(myCar)
